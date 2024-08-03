f7c3327ab4755bc0e24ff256e25b7b1d210d99be
fredy-tkp <fredy.gunawan@tokopedia.com>
2024-08-03 14:20:46 +0700

    feat(fonologi): add localize

---
+++ assets/js/localize.js
@@ -0,0 +1,133 @@
+const translations = {
+    id: {
+      preactivity: "Prakegiatan",
+      competencyMap: "Peta kompetensi",
+      navTitle: "FONOLOGI BAHASA INDONESIA BAGI PEMELAJAR BIPA",
+      indexTitle: "BAHAN AJAR FONOLOGI BAGI PEMELAJAR BIPA KOREA",
+      home: "Beranda",
+      introduction: "Pendahuluan",
+      guideline: "Petunjuk",
+      about: "Tentang",
+      guidelineTitle: "Petunjuk",
+
+      guidelineTable1Title: "Bagi Pemelajar BIPA.",
+      guidelineTable11: "Buka website “Fonologi Bahasa Indonesia bagi Pemelajar BIPA”",
+      guidelineTable12: "Tekan fitur “Peta Kompetensi” untuk memelihat refleksi kompetensi",
+      guidelineTable13: "Tekan fitur “Pasangan Minimal” untuk latihan",
+      guidelineTable14: "Tekan fitur “Materi” untuk melihat semua materi",
+      guidelineTable15: "Pelajari materi dari “Unit” 1 hingga “Evaluasi Akhir”",
+      guidelineTable16: "Bacalah teks yang disajikan secara seksama",
+      guidelineTable17: "Perhatikan gambar-gambar dan transkripsi fonetik yang disediakan",
+      guidelineTable18: "Dengarkan audio yang disajikan secara seksama",
+      guidelineTable19: "Simak dan pahami bunyi pelafalan pasangan minimal yang disajikan",
+      guidelineTable110: "Ikuti pelafalan bunyi setiap kata yang disajikan",
+      guidelineTable111: "Bandingkan pelafalan Anda dengan audio yang disediakan",
+      guidelineTable112: "Lakukan latihan pelafalan secara berulang-ulang dengan fitur speech-to-text yang disediakan",
+
+      guidelineTable2Title: "Bagi Pengajar BIPA",
+      guidelineTable21: "Bagikan tautan website “Fonologi Bahasa Indonesia bagi Pemelajar BIPA” kepada pemelajar",
+      guidelineTable22: "Berikan instruksi kepada pemelajar untuk membaca teks yang disajikan secara seksama",
+      guidelineTable23: "Berikan instruksi kepada pemelajar untuk memperhatikan gambar-gambar dan transkripsi fonetik yang disediakan",
+      guidelineTable24: "Berikan instruksi kepada pemelajar untuk mengarkan audio yang disajikan secara seksama",
+      guidelineTable25: "Berikan instruksi kepada pemelajar untuk menyimak dan memahami bunyi pelafalan pasangan minimal yang disajikan",
+      guidelineTable26: "Berikan instruksi kepada pemelajar untuk mengikuti pelafalan bunyi setiap kata yang disajikan",
+      guidelineTable27: "Bersama dengan pemalajar membandingkan pelafalan pemelajar dengan audio yang disediakan",
+
+      competencyMapTitle: "PETA KOMPETENSI",
+      competencyMapTable1: "KOMPETENSI",
+      competencyMapTable2: "CAPAIAN PEMBELAJARAN",
+      competencyMapTable3: "Menguasai pengetahuan tentang penggunaan bunyi bahasa dan pelafalan.",
+      competencyMapTable4: "Mengenal alat ucap dan cara alat ucap menghasilkan bunyi.",
+      competencyMapTable5: "Mampu melafalkan huruf vokal, konsonan, diftong, dan gabungan konsonan dalam bahasa Indonesia dengan tepat dalam konteks kehidupan sehari-hari.",
+      competencyMapTable6: "Menguasai penggunaan fonem dalam konteks komunikatif",
+      competencyMapTable7: "Memahami bagaimana variasi fonem dapat mempengaruhi makna kata dan pengertian dalam komunikasi sehari-hari.",
+      competencyMapTable8: "Mampu membedakan bunyi dan makna fonem-fonem yang mirip dalam konteks percakapan nyata.",
+
+      introductionTitle: "PENDAHULUAN",
+      introduction1: "Latar Belakang",
+      introduction2: "Bahasa Indonesia bagi Penutur Asing (BIPA) saat ini semakin diminati, terutama bagi pemelajar dari  Korea yang tertarik dengan kesempatan komunikasi luas di Indonesia. Namun, salah satu tantangan bagi pemelajar BIPA asal Korea adalah mengatasi perbedaan fonologis antara bahasa Indonesia dan bahasa Korea. Sistem bunyi yang berbeda ini meliputi pengucapan konsonan, vokal, dan pola intonasi yang mempengaruhi kemampuan pemelajar dalam berbicara Bahasa Indonesia secara akurat dan jelas. Untuk membantu mengatasi tantangan ini, dikembangkanlah sebuah website bahan ajar fonologi dengan teknik shadowing khusus untuk pembelajar BIPA asal Korea.",
+      introduction3: "Teknik shadowing dipilih karena efektivitasnya dalam melatih ketepatan, kecepatan, dan intonasi dalam pengucapan bahasa target. Melalui website ini, pemelajar dapat mengakses berbagai materi pelafalan kata, kalimat sederhana, kalimat kompleks, dan percakapan dalam teks bahasa Indonesia yang dirancang untuk dilatihkan dengan teknik shadowing. Panduan yang detail tentang pengucapan konsonan, vokal, dan intonasi bahasa Indonesia juga disediakan untuk membantu pemelajar memperbaiki kefasihan mereka. Bahan ajar yang dikembangkan juga menyediakan daftar pasangan minimal kata yang dapat digunakan pemelajar untuk membedakan bunyi kata yang mirip.",
+      introduction4: "Website ini juga menawarkan latihan interaktif yang memberikan umpan balik langsung kepada pemelajar, memungkinkan mereka untuk berlatih secara mandiri dan fleksibel sesuai dengan kebutuhan masing-masing. Diharapkan dengan adanya sumber belajar ini, pemelajar BIPA Korea dapat mengembangkan kemampuan pelafalan secara signifikan.",
+      introduction5: "Tujuan",
+      introduction6: "Setelah mempelajari modul ini, diharapkan pemelajar dapat:",
+      introduction7: "Meningkatkan kemampuan fonologis, pemahaman dan produksi bunyi-bunyi bahasa Indonesia;",
+      introduction8: "memperbaiki pelafalan pemelajar yang tepat dan akurat sesuai dengan standar pelafalan bahasa Indonesia;",
+      introduction9: "mendorong pemelajar untuk memahami pelafalan dalam percakapan sehari-hari melalui penggunaan teknik shadowing yang menarik;",
+      introduction10: "membantu pemelajar menghindari kesalahan pengucapan umum yang sering dilakukan oleh penutur asing, melalui latihan intensif dengan teknik shadowing;",
+      introduction11: "membantu pemelajar untuk dapat membedakan makna dari bunyi kata yang mirip dengan pasangan minimal kata; dan",
+      introduction12: "mengembangkan kemampuan pemelajar untuk belajar secara mandiri melalui penggunaan bahan ajar digital yang interaktif dan mudah diakses."
+    },
+    ko: {
+      preactivity: "사전 활동",
+      competencyMap: "역량 맵",
+      navTitle: "BIPA 학습자를 위한 인도네시아 음운론",
+      indexTitle: "한국어 BIPA 학습자를 위한 음운론 교육 자료",
+      home: "홈 페이지",
+      introduction: "소개",
+      guideline: "지침",
+      about: "에 대한",
+
+      guidelineTitle: "지침",
+
+      guidelineTable1Title: "BIPA 학생의 경우.",
+      guidelineTable11: "'BIPA 학생들을 위한 인도네시아 음운론' 웹사이트를 엽니다.",
+      guidelineTable12: "역량 반영을 보려면 '역량 지도' 기능을 누르세요.",
+      guidelineTable13: "연습하려면 '최소 쌍' 기능을 누르세요.",
+      guidelineTable14: "모든 자료를 보려면 '자료' 기능을 누르십시오.",
+      guidelineTable15: "'단원' 1부터 '최종 평가'까지의 학습 자료",
+      guidelineTable16: "제시된 텍스트를 주의 깊게 읽으세요.",
+      guidelineTable17: "제공된 사진과 음성 표기를 살펴보세요.",
+      guidelineTable18: "제시된 오디오를 주의 깊게 들어보세요.",
+      guidelineTable19: "제시된 최소 쌍 발음 소리를 듣고 이해합니다.",
+      guidelineTable110: "제시된 각 단어의 소리 발음을 따르세요.",
+      guidelineTable111: "제공된 오디오와 발음을 비교하세요.",
+      guidelineTable112: "제공되는 음성-텍스트 기능으로 반복적으로 발음을 연습하세요.",
+
+      guidelineTable2Title: "BIPA 교사의 경우",
+      guidelineTable21: "웹사이트 링크 'BIPA 학생을 위한 인도네시아 음운론'을 학생들과 공유하세요.",
+      guidelineTable22: "학생들에게 제시된 텍스트를 주의 깊게 읽도록 지시합니다.",
+      guidelineTable23: "학생들에게 제공된 그림과 음성 표기에 주의를 기울이도록 지시합니다.",
+      guidelineTable24: "학생들에게 주의 깊게 제시된 오디오를 방송하도록 지시합니다.",
+      guidelineTable25: "학생들에게 제시된 최소한의 발음 소리를 듣고 이해하도록 지시합니다.",
+      guidelineTable26: "학습자에게 제시된 각 단어의 소리 발음을 따르도록 지시합니다.",
+      guidelineTable27: "학습자와 함께 제공된 오디오와 학습자의 발음을 비교합니다.",
+
+      competencyMapTitle: "역량 맵",
+      competencyMapTable1: "유능함",
+      competencyMapTable2: "학습 결과",
+      competencyMapTable3: "언어의 소리와 발음 사용에 대한 지식을 습득합니다.",
+      competencyMapTable4: "음성 장치와 음성 장치가 소리를 생성하는 방법에 대해 알아보세요.",
+      competencyMapTable5: "인도네시아어의 모음, 자음, 이중모음 및 자음 조합을 일상생활에서 정확하게 발음할 수 있다.",
+      competencyMapTable6: "의사소통 맥락에서 음소 사용 익히기",
+      competencyMapTable7: "음소 변화가 일상 의사소통에서 단어 의미와 이해에 어떻게 영향을 미칠 수 있는지 이해합니다.",
+      competencyMapTable8: "실제 대화 상황에서 유사한 음소의 소리와 의미를 구별할 수 있습니다.",
+
+      introductionTitle: "소개",
+      introduction1: "배경",
+      introduction2: "BIPA(외국인을 위한 인도네시아어)는 현재 수요가 점점 늘어나고 있으며, 특히 인도네시아에서의 폭넓은 의사소통 기회에 관심이 있는 한국 학생들의 경우 더욱 그렇습니다. 그러나 한국 BIPA 학생들의 과제 중 하나는 인도네시아어와 한국어의 음운학적 차이를 극복하는 것입니다. 이러한 다양한 사운드 시스템에는 학습자가 인도네시아어를 정확하고 명확하게 말하는 능력에 영향을 미치는 자음, 모음 및 억양 패턴의 발음이 포함됩니다. 이러한 어려움을 극복하기 위해 한국의 BIPA 학습자를 위한 특수 쉐도잉 기법을 갖춘 음운론 교재 웹사이트가 개발되었습니다. ",
+      introduction3: "목표 언어 발음의 정확성, 속도 및 억양 훈련의 효율성 때문에 섀도우잉 기술이 선택되었습니다. 이 웹사이트를 통해 학생들은 그림자 기법을 사용하여 연습할 수 있도록 고안된 단어 발음, 간단한 문장, 복잡한 문장, 인도네시아어 텍스트 대화에 대한 다양한 자료에 접근할 수 있습니다. 또한 인도네시아어의 자음, 모음, 억양의 발음에 대한 상세한 지도를 제공하여 학생들의 유창함을 향상시키는데 도움을 줍니다. 개발된 교재는 또한 학생들이 유사한 단어의 소리를 구별하기 위해 사용할 수 있는 최소한의 단어 쌍 목록을 제공합니다.",
+      introduction4: "또한 이 웹사이트는 학습자에게 즉각적인 피드백을 제공하는 대화형 연습을 제공하므로 학습자는 개인의 필요에 따라 독립적이고 유연하게 연습할 수 있습니다. 이 학습 자료를 통해 한국 BIPA 학생들의 발음 능력이 크게 향상될 수 있기를 바랍니다.",
+      introduction5: "목적",
+      introduction6: "이 모듈을 학습한 후 학생들은 다음을 수행할 수 있을 것으로 기대됩니다.",
+      introduction7: "인도네시아어 소리의 음운론적 기술, 이해 및 생성을 향상시킵니다.",
+      introduction8: "인도네시아 발음 표준에 따라 학생들의 정확하고 정확한 발음을 향상시킵니다.",
+      introduction9: "흥미로운 그림자 기술을 사용하여 학생들이 일상 대화에서 발음을 이해하도록 격려합니다.",
+      introduction10: "섀도잉 기술을 집중적으로 연습하여 학습자가 비원어민이 자주 범하는 일반적인 발음 실수를 피할 수 있도록 도와줍니다.",
+      introduction11: "학습자가 최소 단어쌍과 유사한 단어의 소리의 의미를 구별할 수 있도록 도와줍니다.",
+      introduction12: "대화형이며 쉽게 접근할 수 있는 디지털 교육 자료를 사용하여 학생들이 독립적으로 학습할 수 있는 능력을 개발합니다.",
+    }
+  };
+  
+  function loadTranslations(language) {
+    const selectedTranslations = translations[language];
+    updateContent(selectedTranslations);
+  }
+  
+  function setLanguage(language) {
+    loadTranslations(language);
+  }
+  
+  // Set default language to Indonesia on page load
+  document.addEventListener('DOMContentLoaded', () => {
+    setLanguage('id');
+  });
